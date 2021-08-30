d2323725a2c663ef2607f5d77132e9839c417f0b
Saksham Gupta <catchgsaksham@gmail.com>
2021-08-30 08:59:53 -0500

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <p className="text-blue-400">START</p>
+    </div>
+  );
+}
+
+export default App;
