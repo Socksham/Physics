81ce931849bd10505a6e3811e7dfe8768234d4f9
Saksham Gupta <catchgsaksham@gmail.com>
2021-09-02 11:53:08 -0500

    tailwind added?

--- frontend/src/screens/main/Resources.js
+++ frontend/src/screens/main/Resources.js
@@ -3,7 +3,7 @@ import Navbar from '../../components/Navbar'
 import ResourcesCard from '../../components/ResourcesCard'
 import { db } from '../../utils/Firebase'
 
-const Resources = ({history}) => {
+const Resources = ({ history }) => {
     const [data, setData] = useState([])
 
     useEffect(() => {
@@ -20,20 +20,22 @@ const Resources = ({history}) => {
 
     return (
         <>
-            <Navbar history={history}/>
-            
-            <div className="text-center mt-10 mb-10">
-                <p className="text-4xl">Resources</p>
-            </div>
+            <Navbar history={history} />
 
-            <div className="grid grid-cols-3 gap-4 pl-40 pr-40 ">
-                {
-                    data.map((doc) => {
-                        return (
-                            <ResourcesCard img={doc.Image} name={doc.Name} link={doc.Link}/>
-                        )
-                    })
-                }
+            <div className="bg-glass h-screen">
+                <div className="text-center pt-6 mb-20">
+                    <p className="text-4xl">Resources</p>
+                </div>
+
+                <div className="grid grid-cols-3 gap-4 pl-40 pr-40">
+                    {
+                        data.map((doc) => {
+                            return (
+                                <ResourcesCard img={doc.Image} name={doc.Name} link={doc.Link} />
+                            )
+                        })
+                    }
+                </div>
             </div>
         </>
     )
