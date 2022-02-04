5cc25c58215f8797481ee766f0443bd103484ce6
Ronit Anandani <56076011+anandani4136@users.noreply.github.com>
2022-02-04 08:26:01 -0600

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="bg-glass flex justify-center p-4 pt-10">
-            <p className="text-sm">Built By Saksham Gupta, Class of 2023</p>
+            <p className="text-sm">Built By Saksham Gupta '23, Ronit Anandani '22, and Khush Makadia '22</p>
         </div>
     )
 }
